0f7b376ec4b8c68cf331245d9e4ea5c3d36a09af
Juan <jp41011@gmail.com>
2016-02-07 22:54:41 -0800

    Starting assignment 2 which is a build on top of assignment 1 (lab1). Change code (language) so that it can take in ^ (exponent) and ( ) parenthesis properly

--- a1/src/a1.cpp
+++ a1/src/a1.cpp
@@ -1,3 +1,6 @@
+/* CSci 117 - Assignment 1
+ * Juan Pedraza 2/7/16
+ */
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -25,33 +28,24 @@ ifstream fin;
 // main
 int main(int argc, char* argv[])
 {
-	//printf("**** Program Start ****\n");
-	//printf("result=%d\n", Exp());
 
-	// have to open file and save as global var
-	//fin.open(argv[1]);
-	fin.open("p1.data");
+	fin.open("input.txt");
 
 
 	cout << "**** Program Start ****" << endl;
-	/*
-	for(int i=0; i < argc; i++)
-		cout << argv[i] << " - ";
-	*/
+
 	cout << endl << "Result = " << Exp() << endl;
 	return 0;
 }
 
 int Exp()
 {
-	//printf("Exp()\n");
 	cout << "Exp()" << endl;
 	return Exp2(Term()); //
 }
 
 int Term()
 {
-	//printf("Term()\n");
 	cout << "Term()" << endl;
 	return Term2(Fact()); // calls Fact() to get the next char
 }
@@ -64,23 +58,19 @@ If the next char is a + we add our input value inp to what we get from calling T
 */
 int Exp2(int inp)
 {
-	//printf("Exp2(%d)\n", inp);
 	cout << "Exp2(" << inp << ")" << endl;
 	int result=inp;
 	char a;
-	//if((a = getchar()) != '\n') // not at the end of the line
 	if( fin.get(a).eof() == false)
 	{
 		if(a =='+') // if addition
 		{
 			result = Exp2(result + Term()); // return number plus next char
-			//printf("%d + Term() = %d\n", inp, result);
 			cout << inp << " + Term() = " << result << endl;
 		}
 		else if (a =='-') // if subtraction
 		{
 			result = Exp2(result - Term()); // return number minus next char
-			//printf("%d - Term() = %d\n", inp, result);
 			cout << inp << " - Term() = " << result << endl;
 		}
 	}
@@ -99,28 +89,23 @@ If the next char is + or - then we put the char back ...
 */
 int Term2(int inp)
 {
-	//printf("Term2(%d)\n", inp);
 	cout << "Term2(" << inp << ")" << endl;
 	int result = inp;
 	char a;
-	//if((a=getchar()) != '\n') // if not equal to new lin
 	if( fin.get(a).eof() == false )
 	{
 		if(a=='*') // if multiplication
 		{
 			result = Term2(result * Fact()); // return result multiplied by next char
-			//printf("%d * Fact() = %d\n", inp, result);
 			cout << inp << " * Fact() = " << result << endl;
 		}
 		else if(a=='/') // if division
 		{
 			result = Term2(result / Fact()); // return result divided by next char
-			//printf("%d / Fact() = %d\n", inp, result);
 			cout << inp << " / Fact() = " << result << endl;
 		}
 		else if(a == '+' || a=='-') // if addition or subtraction lower precedence
 		{
-			//ungetc(a, stdin); // do what? put char back ... say what?
 			fin.putback(a);
 		}
 	}
@@ -130,15 +115,9 @@ int Term2(int inp)
 // reads in one character at a time
 int Fact()
 {
-	//char a = getchar();
-	//printf("Fact(%c)\n", a); // debug
-
 	char a;
 	//int result;
 	fin.get(a);
 	cout << "Fact(" << a << ")" << endl;
 	return atoi(&a); // char converted to integer
-	//stringstream ss(a);
-	//ss >> result;
-	//return result;
 }
