d13eeb2a853cbf262fc76873f2a79a44c6dd0493
Juan <jp41011@gmail.com>
2016-02-09 21:58:44 -0800

    Finished with CSci 117 - Assignment 2 Tested it with all 3 given test inputs and it worked. Just need to print code and print the output results to hand in.

--- a1/src/a1.cpp
+++ a1/src/a1.cpp
@@ -1,5 +1,5 @@
-/* CSci 117 - Assignment 1
- * Juan Pedraza 2/7/16
+/* CSci 117 - Assignment 2
+ * Juan Pedraza 2/9/16
  */
 #include <iostream>
 #include <fstream>
@@ -16,8 +16,6 @@ Program description:
 
  Input: String of characters from a file descripting mathematical operations (no spaces)
  Output: Evaluate the mathematical operations and will return the result.
-
- Compile: gcc prog1.c -o prog1
 */
 
 // Advanced function declaration
@@ -30,10 +28,10 @@ ifstream fin;
 int main(int argc, char* argv[])
 {
 
-	fin.open("input.txt"); // file from previous lab
+	//fin.open("input.txt"); // testing debugging file
 	//fin.open("input1.txt");
 	//fin.open("input2.txt");
-	//fin.open("input3.txt");
+	fin.open("input3.txt");
 
 	cout << "**** Program Start ****" << endl;
 
@@ -44,20 +42,20 @@ int main(int argc, char* argv[])
 int Exp()
 {
 	cout << "Exp()" << endl;
-	return Exp2(Term()); //
+	return Exp2(Term()); //Exp() calls Exp2() with the value that Term() returns
 }
 
 int Term()
 {
 	cout << "Term()" << endl;
-	return Term2(Fact()); // calls Fact() to get the next char
+	return Term2(Fact()); // Term() calls Term2() with the value returned from Fact()
 }
 
 /*
 Exp2(int inp)
 	int inp: input value integer
 This function will take an integer as input it will then get the next char from the input stream (or file).
-If the next char is a + we add our input value inp to what we get from calling Term()
+If the next char is a + or - we do the respective operations with the current result input and what we get from calling Term()
 */
 int Exp2(int inp)
 {
@@ -75,10 +73,7 @@ int Exp2(int inp)
 		{
 			result = Exp2(result - Term()); // return number minus next char
 			cout << "\t" << inp << " - Term() = " << result << endl;
-		}/*else if(a == ')'){
-			result = Exp2(result);
-			cout << "\t" << inp << " )" << result << endl;
-		}*/
+		}
 	}
 	return result;
 }
@@ -92,6 +87,7 @@ IF it is + or / since * and / has a higher precedence.
 	If it is * or / then we call Fact()  to  get the next char which will be the 2nd number.
 	Then we either * or / and return the result.
 If the next char is + or - then we put the char back ...
+If next char is ) then we also put the char back and return current result
 */
 int Term2(int inp)
 {
@@ -104,46 +100,55 @@ int Term2(int inp)
 		{
 			result = Term2(result * Fact()); // return result multiplied by next char
 			cout << "\t" << inp << " * Fact() = " << result << endl;
+
 		}else if(a=='/') // if division
 		{
 			result = Term2(result / Fact()); // return result divided by next char
 			cout << "\t" << inp << " / Fact() = " << result << endl;
+
 		}else if(a == '+' || a=='-') // if addition or subtraction lower precedence or ')'
 		{
 			fin.putback(a);
 			cout << "\t" << " putback: " << a << endl;
-		}else if( a == ')'){
-				fin.putback(a);
-				cout << "\t" << "putback: " << a << endl;
-				Fact();
+
+		}else if( a == ')'){ // if close parenthesis put char back on stream and
+			cout << "\t putback: )" << endl;
+			fin.putback(a);
+			//result = Exp(); // not sure what to do after you get to the closing parenthesis
 		}
 	}
 	return result;
 }
 
 // reads in one character at a time
+// if next char is ^ then perform operation using the next Fact()
 int Fact()
 {
 	cout << "Fact() - " << endl;;
 	char a;
 	int result = Num();
-	cout << "Fact() - " << endl;
+	cout << "  Fact() - " << endl;
 	fin.get(a);
 	cout << "\t got: " << a << endl;
-	if( a == '^'){
+
+	if( a == '^'){ // if read in ^ then return the result ^ Fact() ... in case Fact() could be a string of other operations
 		cout << "\t result: " << result << " a: " << a << endl;
 		return pow(result, Fact());
 	}else{
 		cout << "\t putback: " << a << endl;
 		fin.putback(a);
-		cout << "\t result: " << result << endl;
+		cout << "\t result: " << result << endl; // show current result
 		//return Fact();
 	}
 	//cout << "\t got:" << a << endl;
 	//return atoi(&a); // char converted to integer
 	return result;
 }
-
+/*Num()
+ * reads in the next char
+ * if next char is ( then the following will be an expression so call Exp()
+ * else then it is a terminal so conver from char to int and return that
+ */
 int Num()
 {
 	cout << "Num() - " << endl;;
@@ -151,7 +156,7 @@ int Num()
 	fin.get(a);
 	if(a == '('){
 		cout << "\t got: (" << endl;
-		return Num(); // not really sure what to do in the case that we read in a '('
+		return Exp(); // call Exp() and return the result of that
 	}else{
 		cout << "\t got: " << a << endl;
 		return atoi(&a);
