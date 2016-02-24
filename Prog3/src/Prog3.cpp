76528a67af05d7715c6fba6a3c3e55031820cbac
Juan <jp41011@gmail.com>
2016-02-23 20:51:35 -0800

    CS 117 - Prog3 Created project for prog3 assignment. Have to look at notes to see everything that needs to be implemented will most likely use previous programming assignments.

---
+++ Prog3/src/Prog3.cpp
@@ -0,0 +1,22 @@
+/*============================================================================
+ Name        : Prog3.cpp
+ Author      : Juan Pedraza
+ Version     : v1 - 2/23/16
+ Copyright   : Your copyright notice
+ Description : CS 117 - Prog3
+ 	 	 	 Task: Build an interpreter for the FresnoSP16 language that was defined.
+ 	 	 	 	 Should check for at least two errors for the following three error classes:
+ 	 	 	 	 	 Lexical -
+ 	 	 	 	 	 Syntax -
+ 	 	 	 	 	 Semantic -
+ 	 	 	 Input: a mini-language FresnoSP16 programming (stored in a data file)
+ 	 	 	 Output: Printout of execution reseult
+============================================================================
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
