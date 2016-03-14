3bb2e1d27a59e7f74ee8199c16e4138139f86cb0
Juan <jp41011@gmail.com>
2016-03-13 22:47:37 -0700

    Finished Prog4. Found 10 duplicate sequences.

--- Prog4/src/Prog4.cpp
+++ Prog4/src/Prog4.cpp
@@ -2,14 +2,14 @@
 ============================================================================
 Name        : Prog4.cpp
 Author      : Juan Pedraza
-Version     : v1 - 3/9/16
+Version     : v2 - 3/13/16
 Copyright   : Your copyright notice
 Description : Programming Assignment 4 - CSci 117 Spring 2016
 				Task: Given a data file of biological sequences in which each sequence (a string)
 				 	 follows its label (a string), find duplicated sequences and rebuild the data file
 				 	 with multiple labels for duplicated sequences
 				 Input: data file with sequences
-				 Output: file with non-dubplicated sequences
+				 Output: file with non-dubplicated sequences. Filename: output.txt
 				 Environment: Eclipse (IDE) Ubuntu - Quad Core i7
 
 ============================================================================
@@ -19,7 +19,7 @@ Description : Programming Assignment 4 - CSci 117 Spring 2016
 #include <fstream>
 #include <vector>
 #include <unordered_map>
-#include <queue>
+//#include <queue>
 
 using namespace std;
 
@@ -29,11 +29,29 @@ typedef unordered_map<string, vector<string> > stringVectorMap;
 fstream inputFile;
 ofstream outputFile;
 
+// Takes in an unordered map and then outputs all the unique keys (seqs) and the id that have those sequences
+// Input: stringVectorMap instance
+// Output: prints out contents no return value
+void outputSeq(stringVectorMap map1){
+	for(auto it=map1.begin(); it != map1.end(); it++)
+	{
+		for(unsigned int i=0; i < it->second.size(); i++)
+		{
+			outputFile << it->second[i] << ", ";
+		}
+		outputFile << endl;
+		outputFile << it->first << endl;
+	}
+}
+
 int main() {
 
 	inputFile.open("Prog4-data");
+	//inputFile.open("test-data.txt");
+
 	outputFile.open("output.txt");
 	stringVectorMap map1;
+	int duplicateCount = 0; // count how many duplicate sequences we find
 
 	string tempID = "";
 	string tempSeq = "";
@@ -42,22 +60,23 @@ int main() {
 		getline(inputFile, tempSeq); // get the sequence (next line)
 		if(map1.count(tempSeq) == 1 ) // found element already in map
 		{ // add the id to the vector of ids that share this sequence
+			duplicateCount++; // increase count of duplicate sequences found
+			cout << "Found duplicate seq add ID to list of IDs: " << tempID << endl;
 			map1[tempSeq].push_back(tempID); // add new id to list
 		}else{ // sequence not already in map
 			//add new sequence to map
+			//cout << "New seq - add ID: " << tempID << endl;
 			vector<string> tempVec; // create temp vector
 			tempVec.push_back(tempID); // add string to vector
 			map1[tempSeq] = tempVec; // add new sequence and id to map
 		}
 	}
-	auto it = map1.begin();
-	cout << it->first << endl;
-	vector<string> tvec = it->second;
-	for(unsigned int i=0; i<tvec.size(); i++)
-		cout << tvec[i] << endl;
 
 	cout << "map1.size() = " << map1.size() << endl;
+	cout << "Duplicate count: " << duplicateCount << endl;
+	outputSeq(map1);
 
+	/*
 	stringMap myMap ( { {"apple", "red"}, {"lemon", "yellow"} } );
 	myMap["wo"] = "2";
 	myMap["three"];
@@ -65,6 +84,7 @@ int main() {
 	{
 		cout << it->first << "\t" << it->second << endl;
 	}
+	*/
 
 	inputFile.close();
 	outputFile.close();
