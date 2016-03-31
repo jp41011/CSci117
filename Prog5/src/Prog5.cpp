01d3c3e6b1f7c6ee142945e392b5c49432ee3c5a
Juan <jp41011@gmail.com>
2016-03-31 01:32:38 -0700

    Finished off working on the delete function.

--- Prog5/src/Prog5.cpp
+++ Prog5/src/Prog5.cpp
@@ -36,21 +36,29 @@ public:
 	bool isMarked(){return mark_bit;}
 
 	// setter functions
-	void setKey(int newKey){key=newKey;}
+	void setKey(int newKey){key=newKey; mark();}
 	void setNext(int newNext){next=newNext;}
 	void mark(){mark_bit=true;}
+	void unMark(){mark_bit=false;}
 
 };
 
 
 
 void intro_options(); // main intro options for user
-void print_memory(); // print summary of memory contents
-void myInsert();  // insert key into a given list
-void myDelete(); // delete key from given list
-void garbage_collect(); // use mark and sweep method to free up memory
 void init_memory(); // initialize memory
-void insertion(int , int); // does the actual insertion
+void print_memory(); // print summary of memory contents
+
+void myInsert();  // insert key into a given list
+void insertion(int& , int); // does the actual insertion
+
+void deleteIntro(); // get information to delete
+void myDelete(int, int); // delete key from given list
+
+void garbage_collect(); // use mark and sweep method to free up memory
+
+void printListIntro();
+void printList(int); // iterate and print list values
 
 vector<Cell> memory;
 
@@ -80,20 +88,23 @@ void intro_options(){
 	cout << "2. Delete" << endl;
 	cout << "3. Garbage Collection" << endl;
 	cout << "4. Print Memory" << endl;
-	cout << "5. Quit" << endl;
+	cout << "5. Print List" << endl;
+	cout << "6. Quit" << endl;
 	cout << "==== ====" << endl;
 	cout << "Command: ";
-
+	
 	cin >> input;
 	if(input == "1"){
 		myInsert();
 	}else if(input == "2"){
-		//myDelete();
+		deleteIntro();
 	}else if(input == "3"){
 		//garbage_collect();
 	}else if(input == "4"){ // print memory
 		print_memory();
-	}else if(input == "5"){ // exit for break out of this
+	}else if(input == "5"){
+		printListIntro();
+	}else if(input == "6"){ // exit for break out of this
 		cout << "[Exit]" << endl;
 		return;
 	}
@@ -164,23 +175,106 @@ void myInsert(){
 
 	if(headFree < 10){ // if there is still at least one free cell
 		memory[headFree].setKey(userValue); // user next free cell and set value
+		memory[headFree].setNext(-1); // just make sure it points to nothing (end of list)
 
 	}else{ // no free cells so have to clear memory and then add value
 		//todo
 	}
 
 	headFree++; // move free pointer to next cell
-	if(userList == '1')
-		insertionSort(head1, headFree-1);
+	if(userList == "1")
+		insertion(head1, headFree-1);
 	else
-		insertionSort(head2, headFree-1);
+		insertion(head2, headFree-1);
 
 	cout << "==== Insert End ====" << endl;
 	return;
 }
 
-// insertion does the actual insertion give list head pointer and insert value
-void insertion(int head, int newIndex){
-	if(head == -1) // empty list
+// insertion does the actual insertion give list head pointer and inserted index
+// does the sorting (insertion sort)
+void insertion(int& head, int newIndex){
+	if(head == -1){ // empty list
+		head = newIndex;
+		return;
+	}
+	//non empty list
+	int curIndex = head; // current index
+	int prevIndex = 0; // keep track of the previous index
+	bool isInserted = false; // flag
+	while(isInserted == false)
+	{
+		if(memory[newIndex].getKey() < memory[curIndex].getKey()) // insert into front of list
+		{
+			memory[newIndex].setNext(curIndex);
+			if(curIndex == head) // its the start of the list (head)
+			{
+				head = newIndex; // update to the new start of the list
+			}else{ // not the front of the list but somewhere in the middle
+				memory[prevIndex].setNext(newIndex); // update last pointer to point to new intermidate cell
+			}
+			isInserted = true;
+		}else if(memory[curIndex].getNext() == -1){ // reached the end of the list
+			//memory[prevIndex].setNext(newIndex); // update last pointer to point to new intermidate cell
+			memory[curIndex].setNext(newIndex);
+			isInserted = true;
+		}else{ //
+			prevIndex = curIndex;
+			curIndex = memory[curIndex].getNext();
+		}
+	}
+}
 
+// intro to print list function. get list which has to be printed
+void printListIntro()
+{
+	cout << "Enter (1): List1 or (2): List2: ";
+	int list;
+	cin >> list;
+	if(list == 1)
+		printList(head1);
+	else
+		printList(head2);
+}
+
+// given a head index this will iterate through the list and print out the values
+void printList(int head)
+{
+	cout << "List: ";
+	int curIndex = head;
+	while(true){
+		if(curIndex == -1) // reached end
+			break;
+		cout << memory[curIndex].getKey() << " ";
+		curIndex = memory[curIndex].getNext();
+	}
+	cout << endl;
+}
+
+// get info about what to delete from user and then call delete function
+void deleteIntro()
+{
+	cout << "Enter (1): List1 or (2): List2: ";
+	int list;
+	cin >> list;
+	cout << "Enter value to delete: ";
+	int value;
+	cin >> value;
+	
+	if(list == 1)
+		myDelete(head1, value);
+	else if (list == 2)
+		myDelete(head2, value);
+	
+	return;
+}
+
+// do the actual delete here TODO
+void myDelete(int head, int val)
+{
+	if(memory[head].getKey() == val) // remove head
+	{
+		
+	}
+	int curIndex = head;
 }
